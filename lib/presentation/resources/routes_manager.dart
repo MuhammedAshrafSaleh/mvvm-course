8a19dfe6cafc97fb535d82fa9cf8fac1bce32186
MuhammedAshrafSaleh <muhammedashrafsaleh@gmail.com>
2025-11-01 08:06:14 +0200

    Finish AppStrings Manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_course/presentation/forget_password/forget_password_view.dart';
+import 'package:mvvm_course/presentation/login/login_view.dart';
+import 'package:mvvm_course/presentation/main/main_view.dart';
+import 'package:mvvm_course/presentation/register/register_view.dart';
+import 'package:mvvm_course/presentation/resources/strings_manager.dart';
+import 'package:mvvm_course/presentation/splash/splash_view.dart';
+import 'package:mvvm_course/presentation/store_details/store_details_view.dart';
+
+class Routes {
+  static const String splashRoute = "/";
+  static const String loginRoute = "/login";
+  static const String registerRoute = "/register";
+  static const String forgetPasswordRoute = "/forgetPassword";
+  static const String mainRoute = "/main";
+  static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: Text(AppStrings.noRouteFound)),
+        body: Center(child: Text(AppStrings.noRouteFound)),
+      ),
+    );
+  }
+}
