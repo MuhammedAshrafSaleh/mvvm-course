aa28cb8f33ab5d45fa34b20252dc36fb71e13f1c
MuhammedAshrafSaleh <muhammedashrafsaleh@gmail.com>
2025-11-01 09:24:24 +0200

    Lecture 22

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,19 @@
 class AppStrings {
   static const String noRouteFound = "No Route Found";
+
+  static const String onBoardingTitle1 = "Discover Your Workflow";
+  static const String onBoardingSubTitle1 =
+      "Organize your projects easily, track progress in real-time, and stay productive anywhere.";
+
+  static const String onBoardingTitle2 = "Customize Your Experience";
+  static const String onBoardingSubTitle2 =
+      "Choose themes that suit your style, set your preferences once, and make the app truly yours.";
+
+  static const String onBoardingTitle3 = "Achieve Your Goals Faster";
+  static const String onBoardingSubTitle3 =
+      "Focus on what really matters, automate repetitive tasks, and reach milestones efficiently.";
+
+  static const String onBoardingTitle4 = "Stay Connected & Updated";
+  static const String onBoardingSubTitle4 =
+      "Get instant notifications, collaborate with your team, and never miss an important update.";
 }
